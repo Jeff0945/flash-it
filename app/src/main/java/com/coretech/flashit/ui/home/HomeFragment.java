0b70b607d29c40f04f230db7750fd36a21d7be45
Mohammad Naim Mariga <mohammadnaim.mariga@g.msuiit.edu.ph>
2023-06-30 23:24:45 +0800

    [FLASH-29] [BE] Make card sets dynamic in home page

--- app/src/main/java/com/coretech/flashit/ui/home/HomeFragment.java
+++ app/src/main/java/com/coretech/flashit/ui/home/HomeFragment.java
@@ -1,18 +1,26 @@
 package com.coretech.flashit.ui.home;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Rect;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.coretech.flashit.AppDatabase;
 import com.coretech.flashit.CardShape;
 import com.coretech.flashit.CardShapeAdapter;
 import com.coretech.flashit.CenterLayoutManager;
+import com.coretech.flashit.DaoCardSets;
+import com.coretech.flashit.ModelCardSets;
 import com.coretech.flashit.R;
 
 import java.util.ArrayList;
@@ -22,7 +30,6 @@ public class HomeFragment extends Fragment {
     private RecyclerView recyclerView;
     private CardShapeAdapter adapter;
     private List<CardShape> cardShapes = new ArrayList<>();
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
@@ -33,16 +40,21 @@ public class HomeFragment extends Fragment {
         recyclerView.setAdapter(adapter);
         recyclerView.addItemDecoration(new ItemSpacingDecoration(24)); // Set the spacing between items
 
-        // Add sample card shapes
-        cardShapes.add(new CardShape("Card Shape 1"));
-        cardShapes.add(new CardShape("Card Shape 2"));
-        cardShapes.add(new CardShape("Card Shape 3"));
+        AppDatabase appDatabase = AppDatabase.getInstance(getContext());
 
-        // Notify the adapter that data has changed
+        AsyncTask.execute(() -> {
+            List<ModelCardSets> cardSets = appDatabase.cardSets().all();
+
+            for(int i = 0; i < cardSets.size(); i++) {
+                cardShapes.add(new CardShape(cardSets.get(i).name));
+            }
+        });
         adapter.notifyDataSetChanged();
+        // Notify the adapter that data has changed
 
         return rootView;
     }
+
 
     private static class ItemSpacingDecoration extends RecyclerView.ItemDecoration {
         private int spacing;
