00281ad863cded2e19b3376eb1ed1e38fa4866ee
Mohammad Naim Mariga <mohammadnaim.mariga@g.msuiit.edu.ph>
2023-07-02 23:48:31 +0800

    [FLASH-47] Delete function in card viewing tab

--- app/src/main/java/com/coretech/flashit/GridAdapter.java
+++ app/src/main/java/com/coretech/flashit/GridAdapter.java
@@ -1,10 +1,18 @@
 package com.coretech.flashit;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -38,14 +46,69 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.GridViewHolder
     }
 
 
-    public static class GridViewHolder extends RecyclerView.ViewHolder {
+    public class GridViewHolder extends RecyclerView.ViewHolder {
         TextView termsTextView;
         TextView descriptionTextView;
+
+        private ImageView deleteButton;
+
+        private long cardSetId;
 
         public GridViewHolder(@NonNull View itemView) {
             super(itemView);
             termsTextView = itemView.findViewById(R.id.termsTextView);
             descriptionTextView = itemView.findViewById(R.id.descriptionTextView);
+            deleteButton = itemView.findViewById(R.id.deleteButton);
+
+            //Delete card
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final Context context = view.getContext();
+                    AppDatabase appDatabase = AppDatabase.getInstance(context);
+
+                    int position = getBindingAdapterPosition();
+
+                    AsyncTask.execute(() -> {
+                        List<ModelCards> card = appDatabase.cards().all();
+
+                        if (position != RecyclerView.NO_POSITION) {
+                            ModelCards cardToDelete = card.get(position);
+
+                            // Prompt the user with a confirmation dialog
+                            ((Activity) context).runOnUiThread(() -> {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                                builder.setTitle("Delete Card")
+                                        .setMessage("Are you sure you want to delete this card?")
+                                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                // Delete the cardSet from the database
+                                                AsyncTask.execute(() -> {
+                                                    appDatabase.cards().delete(cardToDelete);
+                                                    //appDatabase.cards().getCards(cardSetId);
+                                                });
+
+                                                // Remove the cardSetToDelete from the cardSets list
+                                                card.remove(cardToDelete);
+
+                                                // Notify the adapter that data has changed
+                                                cards.remove(position);
+                                                notifyItemRemoved(position);
+                                                notifyItemRangeChanged(position, cards.size());
+
+                                                // Display a success message
+                                                Toast.makeText(context, "Card deleted successfully", Toast.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .setNegativeButton("Cancel", null)
+                                        .show();
+                            });
+                        }
+                    });
+
+                }
+            });
         }
     }
 }
